6f36d05743ae5d973f8a4a9cc40b67dd4ba1f2f9
Stagiaire <stagiaire@T3620-1B1H.nantes.dawan.fr>
2018-11-29 12:34:00 +0100

    Utilisation du service HTTP avec promesse

--- Atelier/store/src/app/products.service.ts
+++ Atelier/store/src/app/products.service.ts
@@ -2,40 +2,35 @@ import { Injectable } from '@angular/core';
 import {Product} from './product';
 import {LoggerService} from './logger.service';
 import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductsService {
-
-  products: Array<Product>;
+  products: Array<Product> = [];
 
   // J'appelle le service de logger pour pouvoir utiliser ici ses méthodes (log)
   // et récupération du service client http via l'injecteur de dépendances
 
   constructor(private logger: LoggerService, private http: HttpClient) {
 
-    this.products = [];
-    this.products.push(new Product());
-      this.products[0].brand = 'New Balance';
-      this.products[0].name = 'u410cb';
-      this.products[0].price = 70;
-      this.products[0].picture = 'nb.jpg';
+    // Envoi d'une requête HTTP GET vers la ressource assets/data.json
+    // et conversion de l'observable en promesse
+    this.http.get('assets/data.json').toPromise()
+      // Traitement de la réponse en cas de succès
+      .then( (response: Product[]) => {
+        this.logger.log(response);
+        for (const product of response) {
+          this.products.push(product);
+        }
 
-      this.products.push(new Product());
-      this.products[1].brand = 'Adidas';
-      this.products[1].name = 'Trainer';
-      this.products[1].price = 90;
-      this.products[1].picture = 'adtrainer.jpg';
+      })
 
-      this.products.push(new Product());
-      this.products[2].brand = 'Vans';
-      this.products[2].name = 'Sk8';
-      this.products[2].price = 110;
-      this.products[2].picture = 'vans.jpg';
-
-      this.logger.log('Création des produits');
+      .catch(reason => {
+        console.log('Woops something\'s wrong', reason)
+    });
 
   }
 }
